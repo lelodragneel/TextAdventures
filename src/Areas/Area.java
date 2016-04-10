bd89acebdb7150e758eae8261cff036b21ab616a
Jeffsor <jeffsor93@hotmail.com>
2016-04-10 16:01:12 -0300

    Cleaned Area class

--- src/Areas/Area.java
+++ src/Areas/Area.java
@@ -1,23 +1,14 @@
 package Areas;
 
-import java.util.ArrayList;
-
 import General.Room;
 
 public abstract class Area {
 
-	/*
-	 * initialize variables
-	 */
+	//Instance variables
 	private String areaName;
-	private ArrayList[][] location;
+	private Room[][] rooms;
 
 	public Area() {
-
-		// create a 3 X 3 matrix to represent the locations of the area
-		// let m be the row number. let n be the column number
-		location[0][0].add(new Room(null, null, null));
-		
 		
 	}
 
