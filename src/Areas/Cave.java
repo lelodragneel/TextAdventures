a3466566a1f4b223677293fbb2cfa619871459bb
Jeffsor <jeffsor93@hotmail.com>
2016-04-10 16:38:24 -0300

    Cave worked on

--- src/Areas/Cave.java
+++ src/Areas/Cave.java
@@ -18,8 +18,8 @@ public class Cave extends Area {
 		rooms[0][1] = new Room(null, null, null, true);
 		rooms[0][2] = new Room(null, null, null, false);
 		rooms[1][0] = new Room(null, null, null, false);
-		rooms[1][1] = new Room(null, null, null, false);
-		rooms[1][2] = new Room(null, new Minion(1, 1, "Troll"), null, false);
+		rooms[1][1] = new Room(new Boss(1, 1, "Bossy guy"), null, null, false);
+		rooms[1][2] = new Room(null, new Minion(1, 1, "Troll"), new Item(1, 1, "[Wizard] Robe"), false);
 		rooms[2][0] = new Room(null,null, null, false);
 		rooms[2][1] = new Room(null, null, null, false);
 		rooms[2][2] = new Room(null, null, null, false);
