f4cd66aed0383b5078c14c8ab24011c5b2651f8d
Jeffsor <jeffsor93@hotmail.com>
2016-04-10 17:50:16 -0300

    Update cave

--- src/Areas/Cave.java
+++ src/Areas/Cave.java
@@ -11,8 +11,9 @@ public class Cave extends Area {
 	private String areaName = "Cave";
 	private Room[][] rooms;
 	
-	public Cave()
+	public Cave(Character player)
 	{
+		
 		rooms = new Room[3][3];
 		rooms[0][0] = new Room(null, null, null, false);
 		rooms[0][1] = new Room(null, null, null, true);
@@ -20,9 +21,15 @@ public class Cave extends Area {
 		rooms[1][0] = new Room(null, null, null, false);
 		rooms[1][1] = new Room(new Boss(1, 1, "Bossy guy"), null, null, false);
 		rooms[1][2] = new Room(null, new Minion(1, 1, "Troll"), new Item(1, 1, "[Wizard] Robe"), false);
-		rooms[2][0] = new Room(null,null, null, true);
+		rooms[2][0] = new Room(null, null, null, true);
 		rooms[2][1] = new Room(null, new Minion(1, 1, "Troll"), new Item(1, 1, "[Thief] Cape"), false);
 		rooms[2][2] = new Room(null, null, null, true);
 	}
 	
+	public void room1()
+	{
+		System.out.println("You enter the Cave and find an empty room. The room has two doorways, one " +
+		"directly across from where you entered and one off to the right. Somehwere further into the cave " +
+		"you hear a menacing voice \"YOU DARE ENTER MY LAIR? TURN BACK NOW OR FACE CERTAIN DEATH!\"");
+	}
 }
