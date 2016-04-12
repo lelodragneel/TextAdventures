7b64734397e171dcc4d544638e24aece8b87c7b9
donne495 <mr495733@dal.ca>
2016-04-12 12:29:49 -0300

    Text  formatting and minor bug fixes

--- src/Areas/City.java
+++ src/Areas/City.java
@@ -36,14 +36,13 @@ public class City extends Area {
 	{
 
 		String direction;
-		
+		boolean directionFound=false;
 		System.out.println("\nLooking FORWARD, the city's wide main street stretches out before you, bathed in shadow from the looming buildings on either side."
 				+ "\nTo your RIGHT, a narrower street winds off through a collection of smaller buildings. Lights can be seen in the distance."
-				+ "\n Choose your path. Enter r to go right or f to go forward.");
+				+ "\nChoose your path. Enter r to go right or f to go forward.");
 		
 		direction = keyboard.next();
-		
-		choose:
+		while(!directionFound){
 			if(direction.equals("f")){
 				room2();
 			}
@@ -53,8 +52,8 @@ public class City extends Area {
 			else{
 				System.out.println("Command not recognized. Type f for forward, or r for right.");
 				direction=keyboard.next();
-				break choose;
 			}
+		}
 		
 	}
 
@@ -62,7 +61,7 @@ public class City extends Area {
 	{
 		
 		String direction;
-		boolean directionFound = false;;
+		boolean directionFound = false;
 		System.out.println("You proceed straight ahead. The buildings seem taller from this angle, but also a lot more… run down?");
 		System.out.println("Something seems to have driven the city's inhabitants away…");
 		
@@ -70,11 +69,10 @@ public class City extends Area {
 		{
 			System.out.println("As you stare thoughtfully upward, something grabs you from behind!");
 			core.fightEnemy(rooms[0][1].getMinion());
-			core.getPlayer().addToInventory(rooms[0][1].getItem());
 		}
-		System.out.println("You sit down on a bit of rubble to recover. Wait... what's that straight ahead?"
-				+ "To your right, a loud voice rings out: Come this way traveller. FACE ME IF YOU DARE!"
-				+ "\nWill you investigate what lies ahead, or accept the challenge?"
+		System.out.println("\nYou sit down on a bit of rubble to recover. Wait... what's that straight ahead?"
+				+ "\nTo your right, a loud voice rings out: \"HOW DARE YOU HARM MY MINION? FACE ME, SO THAT I MAY DEFEAT YOU ONCE AND FOR ALL.\""
+				+ "\n\nWill you investigate what lies ahead, or accept the challenge?"
 				+ "\nType f to move forward and r to move to your right. To go back to the crossroads, type b.");
 		
 		direction = keyboard.nextLine();
@@ -102,13 +100,16 @@ public class City extends Area {
 		
 		String direction;
 		boolean directionFound=false;
-		System.out.println("The street ahead of you is littered with debris. Gingerly, you reach into one of the piles and pull out a SHIELD.");
+		System.out.println("The street ahead of you is littered with debris. ");
 		if(rooms[0][2].getMinion().isAlive())
 		{
+			System.out.println("The shiny thing that caught your eye seems to be buried in the mess. Gingerly, you reach into one of the piles.");
+			core.getPlayer().addToInventory(rooms[0][2].getItem());
 			System.out.println("Before you can even investigate what you've found, another creature leaps out from the shadows.");
+			core.fightEnemy(rooms[0][2].getMinion());
 		}
 		
-		System.out.println("The path veers off to the right. The only other way to go is backward. "
+		System.out.println("\nThe path veers off to the right. The only other way to go is backward. "
 				+ "\nWhat will you do? Type r to go right or b to go back.");
 		direction=keyboard.nextLine();
 		while(!directionFound){
@@ -134,7 +135,7 @@ public class City extends Area {
 		System.out.println("Concrete buildings block your path in every direction but one."
 				+ "The voice speaks again. \"Come adventurer, don't be scared.\""
 				+ "\nSteeling yourself, you make a decision. Will you move to the right and face the boss? Or will you go backward?"
-				+ "Make a choice. Type r to go right, or b to go back to the previous room.");
+				+ "\nMake a choice. Type r to go right, or b to go back to the previous room.");
 		direction=keyboard.nextLine();
 		while(!directionFound){
 			if(direction.equals("r")){
@@ -161,14 +162,14 @@ public class City extends Area {
 		System.out.println("You back into the shadows to assess your situation.");
 		if(rooms[1][0].getMinion().isAlive())
 		{
-			System.out.println("Unfortunately, you are not alone.");
+			System.out.println("Unfortunately, you are not alone.\n");
 			core.fightEnemy(rooms[1][0].getMinion());
 			core.getPlayer().addToInventory(rooms[1][0].getItem());
 		}
-		System.out.println("The road extends in two directions. From the left, a voice taunts: \"Come here! Allow me to introduce myself!\""
-				+ "\nyou may also proceed forward, if you choose. You can also turn back to the crossroads."
+		System.out.println("\nThe road extends in two directions. From the left, a voice taunts: \"Come here! Allow me to introduce myself!\""
+				+ "\nYou may also go forward or turn back to the crossroads, if you choose."
 				+ "\nMake your choice. Type l to go to the left, f to move forward, and b to go back to the previous room.");
-		direction=keyboard.nextLine();
+		direction=keyboard.next();
 		while(!directionFound){
 			if(direction.equals("f")){
 				directionFound=true;
@@ -194,7 +195,7 @@ public class City extends Area {
 		boolean directionFound=false;
 		System.out.println("You hear a creak, and see a door to the left starting to open.");
 		System.out.println("An elderly-looking woman calls out to you. \"You look tired dear, come in for a moment and rest.\""
-				+ "\n Will you trust her? Type y for yes or n for no.");
+				+ "\nWill you trust her? Type y for yes or n for no.");
 		direction=keyboard.nextLine();
 		while(!directionFound){
 			if(direction.equals("y")){
@@ -217,9 +218,13 @@ public class City extends Area {
 		String direction;
 		
 		boolean directionFound=false;
-		System.out.println("Her house is small, but comfortable. \nOnce you are settled with a bowl of stew and a blanket, she tells you about the \"Creature\" that has terrorizing the city's citizens."
-				+ "\nShe also hands you a homemade POTION. How thoughtful.");
-		System.out.println("Sounds like the monster you've been looking for. You thank the woman and walk back out into the night.");
+		if(rooms[2][1].getPotion()){
+			System.out.println("Her house is small, but comfortable. \nOnce you are settled with a bowl of stew and a blanket, she tells you about the \"Creature\" that has terrorizing the city's citizens."
+					+ "\nShe also hands you a homemade POTION. How thoughtful.");
+			core.getPlayer().findPotion();
+			rooms[2][1].setPotion(false);
+			System.out.println("Sounds like the monster you've been looking for. You thank the woman and walk back out into the night.");
+		}
 		System.out.println("The old woman has directed you to the path on the left." +
 		"There is, however, also a fence straight ahead that you could probably jump over, if you wanted to."
 		+ "\nWhat will you do? Type l to go left or f to go forward.");
@@ -246,21 +251,25 @@ public class City extends Area {
 		System.out.println("You keep walking forward, and… oh. It's a cliff. There's nothing here.");
 		System.out.println("You turn to go back and notice that the fence seems a lot higher somehow. Jumping doesn't look like it will do the trick…");
 		System.out.println("You look around for something that might help you escape. You find a POTION in the grass, but admittedly, there isn't much.");
+		core.getPlayer().findPotion();
+		rooms[2][2].setPotion(false);
 		while(!solved){
 			solved=fencePuzzle();
 		}
 		System.out.println("The only way to go is back.");
+		room7();
 	}
 	
 	public void room9()
 	{
 		if(rooms[1][1].getBoss().isAlive()){
-			System.out.println("As you enter the plaza, what initially appeared to be a huge pile of debris starts to quake.");
-			System.out.println("It appears to be an enormous trash creature. \"Ah, welcome pesky traveller!\" it roars."
+			System.out.println("\nAs you enter the plaza, what initially appeared to be a huge pile of debris starts to quake.");
+			System.out.println("It's an enormous trash creature. \"AH, WELCOME PESKY TRAVELLER!\" it roars."
 					+ "\n\"You have proven yourself to be most irksome in coming here. Once I defeat you, I will truly make this city great again!\""
 					+ "\n\nYou prepare yourself for battle.");
 			core.fightEnemy(rooms[1][1].getBoss());
 			System.out.println("After killing the creature you feel somehow stronger. You leave the city in search of the other horrible monsters.");
+			core.chooseArea();
 		}
 		else
 			System.out.println("You enter the plaza to find the creature already dead. Nice.");
