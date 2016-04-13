28e72811b7bb56017715c753f15cc53cde90c311
donne495 <mr495733@dal.ca>
2016-04-13 16:15:10 -0300

    Text formatting

--- src/General/Core.java
+++ src/General/Core.java
@@ -56,10 +56,10 @@ public class Core {
 
 		// introduce game objective
 		System.out.println("Welcome " + player.getName() + " to the world of Alera. You are an adventurer who has returned to their home village.");
-		System.out.println("This morning you went to the woods to gather fire wood for your home. \nHowever, when you returned to the village you found the houses on fire.");
+		System.out.println("This morning you went to the woods to gather firewood for your home. \nHowever, when you returned to the village you found the houses on fire.");
 		System.out.println("Going through the village you see all the villagers have been killed and their homes destroyed. \nArriving to the center of town you see the mayor bleeding on the ground.");
-		System.out.println("Rushing to him you see he is still alive.Helping him sit up he begins to speak.");
-		System.out.println("*Cough* *Cough* " + player.getName() + " it was horrible these four monsters attacked the village.\nThey kept asking where you were! No one told them so they destroyed the village!\nYou must flee this place! Get as far away as possible!");
+		System.out.println("Rushing to him, you see he is still alive. After helping him sit up, he begins to speak.");
+		System.out.println("/n*Cough* *Cough* " + player.getName() + " it was horrible these four monsters attacked the village.\nThey kept asking where you were! No one told them so they destroyed the village!\nYou must flee this place! Get as far away as possible!");
 		System.out.println("With this last message the mayor stops breathing.\nInstead of leaving as the mayor said you follow the trail of destruction heading away from the village.");
 		
 		System.out.println(
